54b9cf714a5553d1e14a3bc2692c22a2a33fe165
youyeke <youyeke@163.com>
2018-10-09 23:15:51 +0800

    决定整体的动画效果

--- src/Kako/kako.js
+++ src/Kako/kako.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import { List } from './components/Support';
+import QueueAnim from 'rc-queue-anim';
+import { List as SupportList } from './components/Support';
+import './index.css';
 
 export default class Kako extends Component {
   state = {
     ready: false,
   }
   componentDidMount(){
-    this.setState({
-      ready: true,
-    });
+    setTimeout( _=> {
+      this.setState({
+        ready: true,
+      });
+    }, 1000);
   }
   render() {
     const { config, ...restProps } = this.props;
+    const { ready } = this.state;
     return (
       <>
-        <List />
+        <QueueAnim
+          interval={ 0 }
+          animConfig={[
+            { opacity: [1, 0], translateY: [0, 50] },
+          ]}
+          animatingClassName={
+            ['queue-anim-entering', 'Kako-display-none']
+          }
+        >
+            { ready ?
+              (
+                <SupportList key="test" />
+              )
+              :(
+                <SupportList key="SupportList" />
+              )
+            }
+        </QueueAnim>
       </>
     );
   }
