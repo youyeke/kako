a59587da9b8e744e4339940f0c739e8fa8547fb0
LLH <youyeke@163.com>
2018-12-26 18:18:46 +0800

    改用 gulp 进行打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,39 @@
 const gulp = require('gulp');
-const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
-// const uglify = require('gulp-uglify');
+const gulpIgnore = require('gulp-ignore');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
-gulp.task('babel', function(){
-  return gulp.src('./src/**/*.js')
-    .pipe(plumber())
-    .pipe(babel())
-    // .pipe(uglify())
-    .pipe(gulp.dest('./lib'))
-})
-
-gulp.task('copy-resource', function() {
-  gulp.src('./src/**/*.css').pipe(gulp.dest('./lib'));
+gulp.task('babel', () => {
+ return gulp.src('./src/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('lib'))
 });
 
-gulp.task('default', ['babel', 'copy-resource']);
+gulp.task('copy-css', function() {
+  return gulp.src('./src/**/*.css').pipe(gulp.dest('./lib'));
+});
+gulp.task('copy-less', function() {
+  return gulp.src('./src/**/*.less').pipe(gulp.dest('./lib'));
+});
+gulp.task('copy-png', function() {
+  return gulp.src('./src/**/*.png').pipe(gulp.dest('./lib'));
+});
+
+const copyResources = gulp.parallel('copy-css', 'copy-less', 'copy-png');
+
+gulp.task('concat-css', () => {
+  return gulp.src('./src/**/*.css')
+    .pipe(concat('index.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('./dist/index.css')
+    .pipe(concat('index.min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', gulp.series('babel', copyResources, 'concat-css', 'minify-css'));
